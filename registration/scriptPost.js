b764de0a81b9c01fd2cbe2c4bbeba6aea8916ae2
Josip Andric <59053824+Fluoclip@users.noreply.github.com>
2023-04-01 15:33:40 +0200

    post req

---
+++ registration/scriptPost.js
@@ -0,0 +1,24 @@
+const url = 'http://localhost:8080/api/postEmployee';
+const formData = new FormData();
+
+// dodajte podatke iz forme u formData objekt
+formData.append('first-name', document.getElementById('first-name').value);
+formData.append('last-name', document.getElementById('last-name').value);
+formData.append('street', document.getElementById('street').value);
+formData.append('city', document.getElementById('city').value);
+formData.append('email', document.getElementById('email').value);
+formData.append('phone', document.getElementById('phone').value);
+formData.append('guard-id', document.getElementById('guard-id').value);
+formData.append('ausbildung', document.getElementById('ausbildung').value);
+
+
+fetch(url, {
+  method: 'POST',
+  body: formData
+})
+.then(response => {
+  console.log(response);
+})
+.catch(error => {
+  console.error(error);
+});
