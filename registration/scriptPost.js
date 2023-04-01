d0cccd4bc3a891a57ccca7f62678ac2e406d5483
Josip Andric <59053824+Fluoclip@users.noreply.github.com>
2023-04-01 23:46:37 +0200

    update

--- registration/scriptPost.js
+++ registration/scriptPost.js
@@ -3,13 +3,14 @@ const formData = new FormData();
 
 // dodajte podatke iz forme u formData objekt
 formData.append('first-name', document.getElementById('first-name').value);
-formData.append('last-name', document.getElementById('last-name').value);
-formData.append('street', document.getElementById('street').value);
 formData.append('city', document.getElementById('city').value);
-formData.append('email', document.getElementById('email').value);
-formData.append('phone', document.getElementById('phone').value);
+formData.append('street', document.getElementById('street').value);
 formData.append('guard-id', document.getElementById('guard-id').value);
+formData.append('email', document.getElementById('email').value);
+formData.append('last-name', document.getElementById('last-name').value);
 formData.append('ausbildung', document.getElementById('ausbildung').value);
+formData.append('phone', document.getElementById('phone').value);
+
 
 
 fetch(url, {
