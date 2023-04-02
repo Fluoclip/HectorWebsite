e5aeee140b3b148a149c74c0548d8f8dc1359ce8
Josip Andric <59053824+Fluoclip@users.noreply.github.com>
2023-04-02 19:54:30 +0200

    test post

--- registration/test.js
+++ registration/test.js
@@ -1,28 +1,7 @@
-const form = document.getElementById('myForm');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  const formData = new FormData(form);
-  const jsonData = {};
+const formEl = document.querySelector('.form');
 
-  // pretvoriti FormData u JSON
-  for (const [key, value] of formData.entries()) {
-    jsonData[key] = value;
-  }
+formEl.addEventListener('submit', event =>{
+    event.preventDefault();
 
-  const url = 'https://example.com/api';
-  fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(jsonData),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    const formData = new formData(formEl);
 });
