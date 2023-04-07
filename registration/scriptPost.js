c12948125a5b3d8e85c3bbfc7fa0ea68c08c8f7c
Josip Andric <59053824+Fluoclip@users.noreply.github.com>
2023-04-07 10:13:50 +0200

    post

--- registration/scriptPost.js
+++ registration/scriptPost.js
@@ -1,25 +1,26 @@
-const url = 'http://localhost:8080/api/postEmployee';
-const formData = new FormData();
 
-// dodajte podatke iz forme u formData objekt
-formData.append('first-name', document.getElementById('first-name').value);
-formData.append('city', document.getElementById('city').value);
-formData.append('street', document.getElementById('street').value);
-formData.append('guard-id', document.getElementById('guard-id').value);
-formData.append('email', document.getElementById('email').value);
-formData.append('last-name', document.getElementById('last-name').value);
-formData.append('ausbildung', document.getElementById('ausbildung').value);
-formData.append('phone', document.getElementById('phone').value);
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(formEl);
+
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+
+    fetch('http://localhost:8081/api/employees',{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify(data)
+    }
+    
+    
+    
+    );
 
 
-
-fetch(url, {
-  method: 'POST',
-  body: formData
-})
-.then(response => {
-  console.log(response);
-})
-.catch(error => {
-  console.error(error);
 });
--- registration/test.js
+++
@@ -1,7 +0,0 @@
-const formEl = document.querySelector('.form');
-
-formEl.addEventListener('submit', event =>{
-    event.preventDefault();
-
-    const formData = new formData(formEl);
-});
